c60f02d585eb59cbaaaa2d23287161b701690187
elmohawk <mrhawk2007@gmail.com>
2026-06-25 20:06:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // ======================================
 
 const SUPABASE_URL =
-    "YOUR_SUPABASE_URL";
+    "https://exjgejujfxejjlbfizgz.supabase.co";
 
 const SUPABASE_KEY =
-    "YOUR_SUPABASE_ANON_KEY";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
 
 const supabaseClient =
     supabase.createClient(
